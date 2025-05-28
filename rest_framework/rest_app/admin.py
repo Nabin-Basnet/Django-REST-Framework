0a75958cb63bd158ab62b670db0860f4c25cdba3
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-28 18:13:52 +0545

    Function-Based Views (@api_views)

--- rest_framework/rest_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import person
-
-# Register your models here.
-admin.site.register(person)
--- rest_framework/rest_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import person
-
-class personSerilizers(serializers.ModelSerializer):
-    class Meta:
-        model=person
-        field='__all__'
