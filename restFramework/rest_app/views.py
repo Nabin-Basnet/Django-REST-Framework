35351d7c25472f155be526e2ea4e17094b7f695d
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-28 21:59:52 +0545

    explain code through comments

--- restFramework/rest_app/views.py
+++ restFramework/rest_app/views.py
@@ -6,48 +6,54 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-# Create your views here.
+# Basic view to test if the project is running
 def home(request):
-    return HttpResponse("Hello! here we are learning django rest_frameeork.")
+    return HttpResponse("Hello! here we are learning django rest_framework.")
 
-@api_view(['GET','POST'])
+# View to handle GET and POST requests for listing all persons and creating a new person
+@api_view(['GET', 'POST'])
 def personView(request):
-    if request.method=='GET':
-        Person=person.objects.all()
-        serializer=personSerilizers(Person,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+    # Handle GET request - Return a list of all persons
+    if request.method == 'GET':
+        Person = person.objects.all()  # Fetch all person records from the database
+        serializer = personSerilizers(Person, many=True)  # Serialize all person data
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method=="POST":
-        serializer=personSerilizers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+    # Handle POST request - Add a new person
+    elif request.method == 'POST':
+        serializer = personSerilizers(data=request.data)  # Deserialize incoming data
+        if serializer.is_valid():  # Validate the data
+            serializer.save()  # Save the new person to the database
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
-    
+        # If data is invalid, return a 400 error with the data
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
-def personDetailView(request,pk):
+# View to handle GET, PUT, and DELETE requests for a single person using their primary key
+@api_view(['GET', 'PUT', 'DELETE'])
+def personDetailView(request, pk):
     try:
-        persons=person.objects.get(pk=pk)
+        # Try to fetch the person with the given primary key (pk)
+        persons = person.objects.get(pk=pk)
     except:
+        # If not found, return 400 Bad Request
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
-    if request.method=="GET":
-        serializer=personSerilizers(persons)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+    # Handle GET request - Return details of a single person
+    if request.method == 'GET':
+        serializer = personSerilizers(persons)  # Serialize the person object
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method=="PUT":
-        serializer=personSerilizers(persons,data=request.data)
+    # Handle PUT request - Update details of the person
+    elif request.method == 'PUT':
+        serializer = personSerilizers(persons, data=request.data)  # Deserialize new data
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
+            serializer.save()  # Save updated person info
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         
-    elif request.method=="DELETE":
-        persons.delete()
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-# def
-    
+    # Handle DELETE request - Delete the person record
+    elif request.method == 'DELETE':
+        persons.delete()  # Delete the person from database
+        return Response(status=status.HTTP_204_NO_CONTENT)  # 204 = No Content (successfully deleted)
