42cc46331188b0810fca95f993fe9ba4e56df805
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-30 21:20:33 +0545

    create new app blog and model with foreign key

--- restFramework/restFramework/settings.py
+++ restFramework/restFramework/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_app',
-    'rest_framework'
+    'rest_framework',
+    'Blogs',
 ]
 
 MIDDLEWARE = [
