31e3332f552d022c1321e54afaccf75dee895201
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-31 07:01:34 +0545

    serializer for blog and comments

---
+++ restFramework/Blogs/serializer.py
@@ -0,0 +1,14 @@
+from .models import Blog,Comment
+from rest_framework import serializers
+
+class commentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Comment
+        fields='__all__'
+
+
+class blogSerializer(serializers.ModelSerializer):
+    # comments=commentsSerializer(many=True,read_only=True)
+    class Meta:
+        model=Blog
+        fields='__all__'
