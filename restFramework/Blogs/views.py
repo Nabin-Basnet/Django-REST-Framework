d227318f154880cdd2d4b87cf448f9c7970eb121
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-31 17:30:42 +0545

    search filter

--- restFramework/Blogs/views.py
+++ restFramework/Blogs/views.py
@@ -3,6 +3,7 @@ from .models import Blog,Comment
 from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializer import blogSerializer,commentsSerializer
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 def home(request):
@@ -12,6 +13,8 @@ class BlogViewSet(viewsets.ModelViewSet):  # Renamed class
     queryset = Blog.objects.all()
     serializer_class = blogSerializer
     lookup_field='pk'
+    filter_backends=[SearchFilter]  #filter searching
+    search_fields=['blog_title','blog_body']
 
 
 class coommentsView(viewsets.ModelViewSet):
