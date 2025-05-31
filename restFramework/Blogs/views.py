174cd66cbcf7dd1c1aef9a4b73747bad84a75bee
nabin1200 <nabinbasnet1200@gmail.com>
2025-05-31 17:35:23 +0545

    ordering filter

--- restFramework/Blogs/views.py
+++ restFramework/Blogs/views.py
@@ -3,7 +3,7 @@ from .models import Blog,Comment
 from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializer import blogSerializer,commentsSerializer
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 # Create your views here.
 def home(request):
@@ -13,8 +13,9 @@ class BlogViewSet(viewsets.ModelViewSet):  # Renamed class
     queryset = Blog.objects.all()
     serializer_class = blogSerializer
     lookup_field='pk'
-    filter_backends=[SearchFilter]  #filter searching
+    filter_backends=[SearchFilter,OrderingFilter]  # search filter are for searching & orderingfilter for order the data by id either bt asc or dec
     search_fields=['blog_title','blog_body']
+    ordering_fields=['id']
 
 
 class coommentsView(viewsets.ModelViewSet):
